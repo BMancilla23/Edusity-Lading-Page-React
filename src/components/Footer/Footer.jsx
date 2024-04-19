85b277cffa093c98b2caa5f311ac68588e5af227
bmancilla23 <bmancilla119@gmail.com>
2024-04-19 10:02:47 -0500

    Componente footer completado

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,12 @@ import './Footer.css'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+      <p className='footer__text'>&copy; 2024 Edusity. Todos los derechos reservados</p>
+      <ul className='footer__info'>
+        <li className='footer__info-item'><a className='footer__info-link' href="">Terms of Services</a></li>
+        <li className='footer__info-item'><a className='footer__info-link' href="">Privacy Policy</a></li>
+      </ul>
+    </footer>
   )
 }
